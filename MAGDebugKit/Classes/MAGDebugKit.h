74bc310728c11930301b86ce27507d543ae8d257
Anton Selyanin <anton.selyanin@gmail.com>
2022-08-12 09:05:50 +0700

    Fix headers for the framework

--- MAGDebugKit/Classes/MAGDebugKit.h
+++ MAGDebugKit/Classes/MAGDebugKit.h
@@ -11,6 +11,20 @@
 
 
 #import "MAGDebugPanel.h"
-
+#import "MAGSettingsPanelVC.h"
+#import "MAGDebugPanelSettingsKeys.h"
+#import "MAGUDSettingsStorage.h"
+#import "MAGPanelTitleCell.h"
+#import "MAGPanelPickerCell.h"
+#import "MAGSandboxBrowserVC.h"
+#import "MAGPanelInputCell.h"
+#import "MAGPanelButtonCell.h"
+#import "MAGRestrictedTextField.h"
+#import "MAGPanelPickerManager.h"
+#import "MAGPanelToggleCell.h"
+#import "MAGPanelSeparator.h"
+#import "MAGPanelCell.h"
+#import "MAGDebugPanelRespondersManager.h"
+#import "MAGSettingsReactor.h"
 
 #endif /* MAGDebugKit_h */
